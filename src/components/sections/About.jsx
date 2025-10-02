7de4dee02c4c49a4352db6bbc5bbfe89313e40fb
ElegbonH <olayorijuinioluwa@gmail.com>
2025-10-02 13:04:11 +0100

    Better User Experience,
    Updated the favicon image and routes

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -6,7 +6,7 @@ import {
 } from "../../data/skills";
 import SEO from "../seo/SEO";
 import "../../styles/components/About.css";
-import mrHPhoto from "../../assets/mr_h.png";
+import mrHPhoto from "/images/mr_h.png";
 
 const About = () => {
   const [isVisible, setIsVisible] = useState(false);
